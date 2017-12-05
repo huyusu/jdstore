71b06b0094fdb3f3742b03c67662abf9b3e8c4f4
huyusu <cctvhuyusu504@gmial.com>
2017-12-05 21:32:15 +0800

    touch app/views/carts/checkout.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,7 @@ resources :carts do
     end
   end
   resources :cart_items
+  resources :orders
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
